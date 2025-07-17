bb1aacf1aed3cd503dd82af5aeda5fcae15163af
abdulakeem-asiru <asiruabdulakeem@gmail.com>
2025-07-17 22:45:56 +0100

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import Home from "./pages/Home"
 import { gsap } from "gsap";
 import { SplitText, ScrollTrigger } from "gsap/all";
 import { ReactLenis, useLenis } from 'lenis/react'
+import { useState } from "react";
+import Preloader from "./components/common/Preloader";
 
 function App() {
+  const [loadingDone, setLoadingDone] = useState(false);
+
    const lenis = useLenis((lenis) => {
     // called every scroll
     console.log(lenis)
@@ -13,6 +17,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
   return (
     <>
       <ReactLenis root />
+      {!loadingDone && <Preloader onComplete={() => setLoadingDone(true)} />}
       <Home />
     </>
   )
---
+++ src/components/common/Preloader.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const Preloader = ({onComplete}) => {
+    const [count, setCount] = useState(0)
+
+    useGSAP(() =>{
+        let interval = setInterval(() =>{
+            setCount((prev) => {
+                if(prev >=100){
+                clearInterval(interval);
+                gsap.to("#preloader", {
+                    opacity : 0,                    
+            duration: 2,
+            ease: "power1.inOut",
+            onComplete: onComplete,
+          });
+          return 100;
+        }
+        return prev + 1;
+            });
+        }, 100);
+    }, [onComplete])
+  return (
+    <section id="preloader"
+    className="fixed inset-0 bg-black bg-[url('/noise.png')] text-white flex items-center justify-center z-50 font-bold">
+           <h1 className=" text-5xl"   style={{ fontFamily: "'Orbitron', sans-serif" }}> {count}%</h1>
+    </section>
+  )
+}
+
+export default Preloader
